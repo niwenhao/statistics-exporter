f61e58946200b96a8c6343159557d0eff3f51ff2
Your Name <you@example.com>
2024-02-02 12:38:15 +0900

    Generate implement code and comment for javadoc

--- src/main/java/jp/co/hyron/stat/statisticsexporter/common/Matrix.java
+++ src/main/java/jp/co/hyron/stat/statisticsexporter/common/Matrix.java
@@ -10,5 +10,101 @@ package jp.co.hyron.stat.statisticsexporter.common;
  * It should has set/get method to set/get a cell of data using row and key or using row and column.
  */
 public class Matrix {
-    
+    	private String[][] data;
+    	private int lastRow = 0;
+    	private static final int EXTEND_SIZE = 100;
+    	
+    	/**
+    	 * Initialize the matrix with key names in first row 
+    	 * @param keys key names
+    	 */
+    	public void initialize(String[] keys) {
+    		data = new String[EXTEND_SIZE][keys.length];
+    		for (int i = 0; i < keys.length; i++) {
+    			data[0][i] = keys[i];
+    		}
+    	}
+    	
+    	/**
+    	 * Add a row of data
+    	 * @param values data values
+    	 */
+    	public void add(String[] values) {
+    		if (lastRow >= data.length) {
+    			extend();
+    		}
+    		for (int i = 0; i < values.length; i++) {
+    			data[lastRow][i] = values[i];
+    		}
+    		lastRow++;
+    	}
+    	
+    	/**
+    	 * Get a row of data
+    	 * @param row row number starting from 1
+    	 * @return row data
+    	 */
+    	public String[] get(int row) {
+    		return data[row - 1];
+    	}
+    	
+    	/**
+    	 * Set value of a cell
+    	 * @param row row number starting from 1
+    	 * @param key key name
+    	 * @param value cell value 
+    	 */
+    	public void set(int row, String key, String value) {
+    		int col = findColumn(key);
+    		data[row - 1][col] = value;
+    	}
+    	
+    	/**
+    	 * Get value of a cell
+    	 * @param row row number starting from 1  
+    	 * @param key key name
+    	 * @return cell value
+    	 */
+    	public String get(int row, String key) {
+    		int col = findColumn(key);
+    		return data[row - 1][col];
+    	}
+    	
+    	/**
+    	 * Set value of a cell
+    	 * @param row row number starting from 1
+    	 * @param col column number starting from 1
+    	 * @param value cell value
+    	 */
+    	public void set(int row, int col, String value) {
+    		data[row - 1][col - 1] = value;
+    	}
+    	
+    	/**
+    	 * Get value of a cell
+    	 * @param row row number starting from 1
+    	 * @param col column number starting from 1
+    	 * @return cell value
+    	 */
+    	public String get(int row, int col) {
+    		return data[row - 1][col - 1];
+    	}
+    	
+    	private int findColumn(String key) {
+    		for (int i = 0; i < data[0].length; i++) {
+    			if (data[0][i].equals(key)) {
+    				return i;
+    			}
+    		}
+    		return -1;
+    	}
+    	
+    	private void extend() {
+    		String[][] newData = new String[data.length + EXTEND_SIZE][];
+    		for (int i = 0; i < data.length; i++) {
+    			newData[i] = data[i];
+    		}
+    		data = newData;
+    	}
+    }
 }
