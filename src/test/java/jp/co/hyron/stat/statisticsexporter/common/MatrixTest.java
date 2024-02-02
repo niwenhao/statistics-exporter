e738d1c441020b40297a3960cbe3e1356e93079a
Your Name <you@example.com>
2024-02-02 13:39:40 +0900

    prepare instructions

--- src/test/java/jp/co/hyron/stat/statisticsexporter/common/MatrixTest.java
+++ src/test/java/jp/co/hyron/stat/statisticsexporter/common/MatrixTest.java
@@ -15,4 +15,20 @@ class MatrixTest {
         assertArrayEquals(keys, matrix.getData()[0]);
     }
 
+    // TODO: add test method for Matrix.add()
+    //       1. create a Matrix instance use createInstance()
+    //       2. add a row of data using add()
+    //       3. assert the data array has a new row
+    //       4. assert the data array has the same size as the key names array
+    //       5. assert the data array has the same key names as the key names array
+    //       6. assert the data array has the same data as the key names array
+    //       7. assert the lastRow is 1
+
+    // TODO: add test method for Matrix.add()
+    //       1. create a Matrix instance use createInstance()
+    //       2. add many rows over EXTEND_SIZE of data using add()
+    //       3. assert the data array has extended by EXTEND_SIZE rows
+    //       4. assert second row of data array is the same as the first row of data
+    //       5. assert last row is correct.
+
 }
