f3f5cc057e87bec0840c4d1745b00be9fdbe5cb8
Your Name <you@example.com>
2024-02-02 17:27:32 +0900

    Add require for Exporter

--- src/main/java/jp/co/hyron/stat/statisticsexporter/common/prometheus/Exporter.java
+++ src/main/java/jp/co/hyron/stat/statisticsexporter/common/prometheus/Exporter.java
@@ -5,9 +5,12 @@ package jp.co.hyron.stat.statisticsexporter.common.prometheus;
  * 
  * This class should register a metric to micrometer.
  * It has a Extractor reference to get Matrix data.
- * It keeps the name, label name and value pairs in his attributes.
- * It also keeps the label name and key of the matrix pairs, take the label value form Matrix using the key.
- * It also has a metricKey to get the metric value from the matrix.
+ * It keeps the name, label name and value pairs in his attributes to present the predefined metric labels to Prometheus.
+ * It also keeps the label name and key pairs, take the label value form Matrix using the key to present the variable 
+ *   metric labels to Prometheus.
+ * It also has a metricValueKey to get the metric value from the matrix.
+ * 
+ * It also has a register method to register the metric to micrometer.
  */
 public class Exporter {
 
