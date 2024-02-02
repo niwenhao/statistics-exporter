605ab2a835507a1541e087216fb95a0fd5620176
Your Name <you@example.com>
2024-02-02 11:38:31 +0900

    up

--- src/main/java/jp/co/hyron/stat/statisticsexporter/common/Matrix.java
+++ src/main/java/jp/co/hyron/stat/statisticsexporter/common/Matrix.java
@@ -2,11 +2,11 @@ package jp.co.hyron.stat.statisticsexporter.common;
 /**
  * A class to contain data to transfer between components
  * It has a two dimensional array of Strings. The first row is the key names for the data. the following rows are the data.
+ * It should has a lastRow to indicate the number of rows and auto extends the array by 100 rows when lastRow will over Array size.
  * It should has a initialize method to setup the key names without data.
  * It should has a add method to add a row of data.
  * It should has a get method to remove a row of data.
  * It should has set/get method to set/get a cell of data using row and key or using row and column.
- * When the row will be appended, it should check the index range, if the index will overflow, it should expand the array by 100 rows
  */
 public class Matrix {
     
